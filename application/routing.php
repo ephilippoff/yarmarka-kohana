d05941676d145fb6ee52140492cb0b17bf6568c2
Mikhail Makeyev <mihail.makeev@gmail.com>
2013-04-18 11:43:32 +0300

    backend routing

--- application/routing.php
+++ application/routing.php
@@ -1,5 +1,16 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+// close admin controllers
+Route::set('admin', '<controller>(/<action>)', array('controller' => '(admin_.*|Admin_.*)'))->filter(function($route, $params, $request){
+	throw new HTTP_Exception_404;
+});
+// backend routing
+Route::set('backend', 'khbackend(/<controller>(/<action>(/<id>)))')
+	->defaults(array(
+		'directory'  => 'Admin',
+		'controller' => 'Users',
+		'action'     => 'index',
+	));
 /**
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
