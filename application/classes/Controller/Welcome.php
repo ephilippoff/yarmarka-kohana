cba62a3622a123c61657cd85883db7783f8d8806
Mikhail Makeyev <mihail.makeev@gmail.com>
2013-05-16 10:04:49 +0300

    clear welcome controller

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -4,11 +4,6 @@ class Controller_Welcome extends Controller_Template {
 
 	public function action_index()
 	{
-		$user = ORM::factory('User', 318069);
-		if ($user->loaded())
-		{
-			var_dump($user->fullname);
-		}
 	}
 
 } // End Welcome
