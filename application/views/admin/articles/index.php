7fbf355a1a7f29c5f9194be2fd8abc783134c501
Mikhail Makeyev <mihail.makeev@gmail.com>
2013-07-09 20:38:37 +0300

    доработки бэкенда

--- application/views/admin/articles/index.php
+++ application/views/admin/articles/index.php
@@ -28,15 +28,17 @@
 </tr>
 <?php foreach ($articles as $article) : ?>
 <tr>
-	<td><?=$article['id']?></td>
-	<td><?=$article['seo_name']?></td>
+	<td><?=$article->id?></td>
 	<td>
-		<?=str_repeat('&rarr;', $article['level']).$article['title']?>
+		<a href="<?=URL::site(Route::get('article')->uri(array('seo_name' => $article->seo_name)))?>" target="_blank"><?=$article->seo_name?></a>
 	</td>
-	<td><?=$article['created']?></td>
 	<td>
-		<a href="<?=Url::site('khbackend/articles/edit/'.$article['id'])?>" class="icon-pencil"></a>
-		<a href="<?=Url::site('khbackend/articles/delete/'.$article['id'])?>" class="icon-trash delete_article"></a>
+		<?=str_repeat('&rarr;', $article->level).$article->title?>
+	</td>
+	<td><?=$article->created?></td>
+	<td>
+		<a href="<?=Url::site('khbackend/articles/edit/'.$article->id)?>" class="icon-pencil"></a>
+		<a href="<?=Url::site('khbackend/articles/delete/'.$article->id)?>" class="icon-trash delete_article"></a>
 	</td>
 </tr>
 <?php endforeach; ?>
