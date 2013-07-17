c0d429f079197ab51bbbd642be819d49b7948ca9
Mikhail Makeyev <mihail.makeev@gmail.com>
2013-07-17 11:10:23 +0300

    remove debug code

--- application/classes/Controller/Admin/Welcome.php
+++ application/classes/Controller/Admin/Welcome.php
@@ -4,7 +4,6 @@ class Controller_Admin_Welcome extends Controller_Admin_Template {
 
 	public function action_index()
 	{
-		var_dump(Kohana::$config->load('common.main_domain'));
 	}
 
 } // End Admin_Welcome
