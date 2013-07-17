689987ba989b27fac7f3fa8612a3f47b61433506
almaznv <almaznv@bitbucket.org>
2013-07-17 09:52:42 +0600

    git ignore

--- application/config/common.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
-return array(
-	'mail_sending'		=> Kohana::$environment == Kohana::PRODUCTION,	// disabling mail sending for development may be usefull
-	'main_domain'		=> 'yarmarka.dev',
-	'default_region_id'	=> 73,
-	'days_count_between_service_up' => 7,
-);
