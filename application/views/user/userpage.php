74a62e1d9040195dab405f36ec0a88b718f144e0
iden <gippon2008@mail.ru>
2013-09-25 14:26:09 +0600

    Карточка пользователя. Правка заголовка

--- application/views/user/userpage.php
+++ application/views/user/userpage.php
@@ -34,7 +34,13 @@
 			</div>
 		</div>
 		<div class="hheader persomal_room-header ta-c">
-			<h1 style="font-size: 20px" class="ta-c d-in"><?=$user->org_name?></h1>
+			<h1 style="font-size: 20px" class="ta-c d-in">
+						<?php if (empty($user->org_name)) : ?>
+								Страница компании №<?=$user->id?>
+						<?php else : ?>
+								<?=$user->org_name?> 
+						<?php endif; ?>					
+			</h1>
 			<a class="bnt-go-back" href="/" rel='nofollow'><span class="text">На главную</span></a>
 			<?php if ($user->id == Auth::instance()->get_user()->id) : ?><a class="bnt-go-back" href="<?=URL::site('user/profile')?>" rel='nofollow'><span class="text">Редактировать</span></a><?php endif; ?>
 		</div>
