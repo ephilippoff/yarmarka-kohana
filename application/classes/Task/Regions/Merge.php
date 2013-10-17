32e6e95a00cc1d6e9efe4da501c564711e953432
Mikhail Makeyev <mihail.makeev@gmail.com>
2013-10-17 13:07:47 +0300

    таски для городов и регионов

--- application/classes/Task/Regions/Merge.php
+++ application/classes/Task/Regions/Merge.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Task_Regions extends Minion_Task
+class Task_Regions_Merge extends Minion_Task
 {
 	protected $_options = array(
 		'limit' => 1000,
@@ -19,27 +19,35 @@ class Task_Regions extends Minion_Task
 			'('.round($offset/($total/100)).'%)'
 		);
 
-		$unique_regions = array();
+		$unique_regions 	= array();
+		$to_delete_regions 	= array();
+
 		$regions = $this->get_regions($limit, $offset);
 		while(count($regions))
 		{
 			foreach ($regions as $region) 
 			{
-				if ( ! isset($unique_regions[$region->kladr_id]))
+				if ( ! isset($unique_regions[$region->kladr_id]) AND ! isset($to_delete_regions[$region->id]))
 				{
 					$unique_regions[$region->kladr_id] = $region->id;
 				}
 				else // дубль
 				{
-					$affected_rows = $this->relink_cities($region->id, $unique_regions[$region->kladr_id]);
-					$region->delete();
+					$this->relink_cities($region->id, $unique_regions[$region->kladr_id]);
+					$to_delete_regions[$region->id] = $region->id;
 				}
-
 			}
 
 			$offset += $limit;
-			Minion_CLI::write_replace('Processed:'.Minion_CLI::color(count($regions), 'cyan'));
+			Minion_CLI::write_replace('Processed:'.Minion_CLI::color($offset, 'cyan'));
 			$regions = $this->get_regions($limit, $offset);
+		}
+
+		// удаляем по 100 штук
+		foreach (array_chunk($to_delete_regions, 100) as $ids)
+		{
+			$query = DB::query(Database::DELETE, "DELETE FROM region_new WHERE id IN (".join(',', $ids).")");
+			$query->execute();
 		}
 
 		Minion_CLI::write();
@@ -52,7 +60,6 @@ class Task_Regions extends Minion_Task
 			->offset($offset)
 			->limit($limit)
 			->order_by('id')
-			->group_by('kladr_id', 'id')
 			->find_all();
 	}
 
