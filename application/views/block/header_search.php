4c341dc7abf66e9dbb298bb93561f79c2c51510d
iden <gippon2008@mail.ru>
2013-11-12 14:10:37 +0600

    Шапка. Правка шаблона

--- application/views/block/header_search.php
+++ application/views/block/header_search.php
@@ -1,6 +1,7 @@
 <div class="title"></div>
-<div class="btn-red btn-find" onClick="$('#search-form').submit()"><span>Найти</span></div>
+
 <form action="http://<?=Region::get_current_domain()?>/search" method="get" name="search-form" id="search-form">
+<div class="btn-red btn-find" onClick="$('#search-form').submit()"><span>Найти</span></div>	
 <div class="seach-bl-fix">
 	<div class="seach-bl">
 		<select class="cusel-plagin" name="" id="sd1" onChange="$('#search-form').attr('action', this.value)">
