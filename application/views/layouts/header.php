9711241cadbbd71d01a50455d007a43ca3042c96
iden <gippon2008@mail.ru>
2013-11-12 15:01:01 +0600

    Шапка. Фикс. Скрываем меню для неавтор. поль-ей

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -2,26 +2,28 @@
 	<section class="winner">
 		<div class="m_poll z3">
 			<div class="top-line ">
-				<div class="enter-bl logon iLight"><span class="personl-room iLight-nav"><span>Личный кабинет</span></span>
-					<div class="proom-list iLight-cont">
-						
-							<ul>
-								<li><a href="<?=URL::site('user/published')?>"><div class="img"><img src="<?=URL::site('images/pr1.png')?>" alt="" /></div>Мои объявления</a></li>
-								<li><a href="<?=URL::site('user/subscriptions')?>"><div class="img"><img src="" alt="" /></div>Мои подписки</a></li>
-								<li><a href="<?=URL::site('user/favorites')?>"><div class="img"><img src="" alt="" /></div>Мои избранные</a></li>
-								<li><a href="<?=URL::site('user/profile')?>"><div class="img"><img src="<?=URL::site('images/pr2.png')?>" alt="" /></div>Личные данные</a></li>
-								<li><a href="<?=URL::site('user/invoices')?>"><div class="img"><img src="" alt="" /></div>История услуг</a></li>
-								<li><a href="<?=URL::site('user/newspapers')?>"><div class="img"><img src="" alt="" /></div>Купленные газеты</a></li>
+				<?php if (Auth::instance()->get_user()) : ?>
+						<div class="enter-bl logon iLight"><span class="personl-room iLight-nav"><span>Личный кабинет</span></span>
+							<div class="proom-list iLight-cont">
 
-								<?php if (Request::current()->action() != 'userpage' and Auth::instance()->get_user()) : ?>
-									<?php if (Auth::instance()->get_user()->org_type == 2) : ?><li class="last"><a class="green" href="/users/<?=Auth::instance()->get_user()->login?>"><div class="img"><!--<img src="" alt="" />--></div>Страница компании</a></li><?php endif; ?>
-								<?php endif; ?>
+									<ul>
+										<li><a href="<?=URL::site('user/published')?>"><div class="img"><img src="<?=URL::site('images/pr1.png')?>" alt="" /></div>Мои объявления</a></li>
+										<li><a href="<?=URL::site('user/subscriptions')?>"><div class="img"><img src="" alt="" /></div>Мои подписки</a></li>
+										<li><a href="<?=URL::site('user/favorites')?>"><div class="img"><img src="" alt="" /></div>Мои избранные</a></li>
+										<li><a href="<?=URL::site('user/profile')?>"><div class="img"><img src="<?=URL::site('images/pr2.png')?>" alt="" /></div>Личные данные</a></li>
+										<li><a href="<?=URL::site('user/invoices')?>"><div class="img"><img src="" alt="" /></div>История услуг</a></li>
+										<li><a href="<?=URL::site('user/newspapers')?>"><div class="img"><img src="" alt="" /></div>Купленные газеты</a></li>
 
-								<li class="last"><a href="<?=CI::site('user/logout')?>"><div class="img"><img src="<?=URL::site('images/pr3.png')?>" alt="" /></div>Выход</a></li>
-							</ul>
-						
-					</div>
-				</div>
+										<?php if (Request::current()->action() != 'userpage') : ?>
+											<?php if (Auth::instance()->get_user()->org_type == 2) : ?><li class="last"><a class="green" href="/users/<?=Auth::instance()->get_user()->login?>"><div class="img"><!--<img src="" alt="" />--></div>Страница компании</a></li><?php endif; ?>
+										<?php endif; ?>
+
+										<li class="last"><a href="<?=CI::site('user/logout')?>"><div class="img"><img src="<?=URL::site('images/pr3.png')?>" alt="" /></div>Выход</a></li>
+									</ul>
+
+							</div>
+						</div>
+				<?php endif; ?>
 				
 				<?=Request::factory('block/header_region')->execute()?>
 				<?php if ($user = Auth::instance()->get_user()) : ?>
