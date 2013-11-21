490784ce8c3a6a261f8abc91a46cbb284cddbf10
iden <gippon2008@mail.ru>
2013-11-21 17:35:49 +0600

    Смена ключа SMS(перенесен из CI new)

--- application/config/sms.php
+++ application/config/sms.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 return array(
-	'api_key' => '2ITL2NU14E41B12D5G877G6A559B68ULL5KNNU0HZQ68X06Z2LVWS5MM6385ZL17',
-	'from' => '79177145656',
+	'api_key' => '62GEY2464R4Z7O3LE8KAH6K1A991Z0FLFUIY1HVKIIGZFPM73332UCY4V5HIO997',
+	'from' => 'yarmarka',
 );
