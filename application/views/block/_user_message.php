f5aea226e0e848f2d03b88f901cfd22623fe19f7
iden <gippon2008@mail.ru>
2013-12-20 13:14:13 +0600

    Плашка с сообщением для пользователей. Правка.

--- application/views/block/_user_message.php
+++ application/views/block/_user_message.php
@@ -1,4 +1,10 @@
-<?php if ($user_text_enable === 1 and !empty($user_text) and !isset($_COOKIE['disable_message'])) : ?>
+<?php 
+$cookie_condition = isset($_COOKIE['disable_message']) ? $_COOKIE['disable_message'] != md5($user_text) : true;
+
+if ($user_text_enable === 1 and !empty($user_text) and $cookie_condition) : ?>
+			<script>
+ 				var disable_message_key = "<?=md5($user_text)?>";
+			</script>			
 			<div class="user-message-block fn-user-message-block">
 				<div class="user-message-text">
 					<p><?=$user_text?></p>
