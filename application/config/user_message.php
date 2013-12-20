c4e77bc5d819adc62aa97ece255b85503c2b0d07
iden <gippon2008@mail.ru>
2013-12-20 13:25:13 +0600

    Настройка gitignore

--- application/config/user_message.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
-//Сообщение для пользователя: текст сообщения и разрешение на его показ
-return array(
-    'user_message' => array(
-        'user_text' => '',
-        'user_text_enable' => 0
-        )    
-);
