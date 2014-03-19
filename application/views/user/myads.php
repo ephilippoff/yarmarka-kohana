6e8ca12f2c52ca604258e6a85b4872bf9c2c0464
iden <gippon2008@mail.ru>
2014-03-19 11:40:06 +0600

    ЛК. Кнопка услуг

--- application/views/user/myads.php
+++ application/views/user/myads.php
@@ -352,7 +352,8 @@
 								<div class="pmenu ">
 									<ul>
 										<?php if (!$ad->is_bad and !$ad->in_archive and $ad->is_published) : ?>
-													<li><a title="Выделить объявление" href="<?=CI::site('billing/services_for_ads/'.$ad->id)?>" class="btn-pmenu "><i class="ico show"></i><span>Выделить</span></a></li>
+													<li><a title="Воспользоваться услугами" href="<?=CI::site('billing/services_for_ads/'.$ad->id)?>" class="btn-pmenu "><i class="ico show"></i><span>Услуги</span></a></li>
+													<li><a title="Подать в газету" href="<?=CI::site('billing/pay_service/34/'.$ad->id)?>" class="btn-pmenu "><i class="ico show"></i><span>В газету</span></a></li>
 										<?php endif; ?>
 									</ul>
 								</div>
