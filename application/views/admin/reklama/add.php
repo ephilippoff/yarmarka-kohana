65e9073855ccaa56d06b511ef6cb37aeac2fa3d4
iden <gippon2008@mail.ru>
2014-06-11 10:32:00 +0600

    #yarmarka-28 Сделать временной интервал для показа рекламы

--- application/views/admin/reklama/add.php
+++ application/views/admin/reklama/add.php
@@ -2,12 +2,21 @@
 <?=HTML::style('bootstrap/datepicker/css/datepicker.css')?>
 
 <script type="text/javascript" charset="utf-8">
+$(document).ready(function() {
+	// enable datepicker
+	$('.dp').datepicker({
+		format:	'yyyy-mm-dd'
+	}).on('changeDate', function(){
+		$(this).datepicker('hide');
+	});	
+});
+
 
 </script>
 
 <form class="form-horizontal" method="post" enctype="multipart/form-data">
 	<div class="control-group <?=Arr::get($errors, 'title') ? 'error' : ''?>">
-		<label class="control-label">Title</label>
+		<label class="control-label">Заголовок:</label>
 		<div class="controls">
 			<input type="text" value="<?=Arr::get($_POST, 'title', @$ad_element->title)?>" class="input-block-level" name="title" id="title"  />
 			<span class="help-inline"><?=Arr::get($errors, 'title')?></span>
@@ -15,7 +24,7 @@
 	</div>
 
 	<div class="control-group <?=Arr::get($errors, 'link') ? 'error' : ''?>">
-		<label class="control-label">link</label>
+		<label class="control-label">Ссылка:</label>
 		<div class="controls">
 			<input type="text" value="<?=Arr::get($_POST, 'link', @$ad_element->link)?>" class="input-block-level" name="link" id="title"  />
 			<span class="help-inline"><?=Arr::get($errors, 'link')?></span>
@@ -23,37 +32,46 @@
 	</div>
 		
 	<div class="control-group <?=Arr::get($errors, 'class') ? 'error' : ''?>">
-		<label class="control-label">Link color:</label>
+		<label class="control-label">Цвет ссылки:</label>
 		<div class="controls">
 			<?=Form::select('class', array('black' => 'Черный', 'red' => 'Красный', 'green' => 'Зеленый'), Arr::get($_POST, 'class', @$ad_element->class)) ?>
 		</div>	
 	</div>
 	
 	<div class="control-group <?=Arr::get($errors, 'class') ? 'error' : ''?>">
-		<label class="control-label">Cities:</label>
+		<label class="control-label">Города:</label>
 		<div class="controls">
 			<?=Form::select('cities[]', array(1 => 'Тюменская область', 1919 => 'Тюмень', 1947 => 'Нефтеюганск', 1948 => 'Нижневартовск', 1979 => 'Сургут'), null, array('multiple', 'size' => 5)) ?>
 		</div>	
 	</div>	
 
 	<div class="control-group <?=Arr::get($errors, 'class') ? 'error' : ''?>">
-		<label class="control-label">Groups:</label>
+		<label class="control-label">Группы:</label>
 		<div class="controls">
 			<?=Form::select('reklama_group[]', $reklama_group, null, array('multiple', 'size' => 10)) ?>
 		</div>	
 	</div>		
 	
 	<div class="control-group only2" >		
-		<label class="control-label">Banner:</label>
+		<label class="control-label">Баннер:</label>
 		<div class="controls">
 			<input type="file" class="input-small" placeholder="banner" name="image" >
 		</div>		
 	</div>
-
+	
+	<div class="control-group only2" >		
+		<label class="control-label">Интервал для показа:</label>
+		<div class="controls">
+			от
+			<input type="text" class="input-small dp" placeholder="от" name="start_date" value="<?=Arr::get($_POST, 'start_date', date('Y-m-d'))?>">
+			до
+			<input type="text" class="input-small dp" placeholder="до" name="end_date" value="<?=Arr::get($_POST, 'end_date', date('Y-m-d', strtotime('+7 days')))?>">
+		</div>		
+	</div>
 
 	<div class="control-group">
 		<div class="controls">
-			<button type="submit" class="btn">Save</button>
+			<button type="submit" class="btn">Сохранить</button>
 			<!--<button type="reset"  class="btn">Reset</button>-->
 		</div>
 	</div>
--- application/views/admin/reklama/index.php
+++ application/views/admin/reklama/index.php
@@ -1,27 +1,31 @@
 <table class="table table-hover table-condensed articles">
-<tr>
-	<th>Id</th>
-	<th>Title</th>
-	<th>Link</th>
-	<th>Image</th>
-	<th>Class</th>
-	<th></th>
-</tr>
-<?php foreach ($ads_list as $ads_element) : ?>
-<tr>
-	<td><?=$ads_element->id?></td>
-	<td><?=$ads_element->title?></td>
-	<td><?=$ads_element->link?></td>
-	<td>
-		<?php if (is_file(DOCROOT.'uploads/banners/'.$ads_element->image)) : ?>
-				<img src="<?='/uploads/banners/'.$ads_element->image?>" />
-		<?php endif;?>
-	</td>
-	<td><?=$ads_element->class?></td>
-	<td>
-		<!--<a href="<?=Url::site('khbackend/reklama/edit/'.$ads_element->id)?>" class="icon-pencil"></a>-->
-		<a href="<?=Url::site('khbackend/reklama/delete/'.$ads_element->id)?>" class="icon-trash delete_article"></a>
-	</td>
-</tr>
-<?php endforeach; ?>
+	<tr>
+		<th>Id</th>
+		<th>Номер</th>
+		<th>Ссылка</th>
+		<th>Баннер</th>
+		<th>Цвет</th>
+		<th>Дата старта</th>
+		<th>Дата окончания</th>
+		<th></th>
+	</tr>
+	<?php foreach ($ads_list as $ads_element) : ?>
+		<tr>
+			<td><?=$ads_element->id?></td>
+			<td><?=$ads_element->title?></td>
+			<td><?=$ads_element->link?></td>
+			<td>
+				<?php if (is_file(DOCROOT.'uploads/banners/'.$ads_element->image)) : ?>
+						<img src="<?='/uploads/banners/'.$ads_element->image?>" />
+				<?php endif;?>
+			</td>
+			<td><?=$ads_element->class?></td>
+			<td><?=$ads_element->start_date?></td>
+			<td><?=$ads_element->end_date?></td>
+			<td>
+				<!--<a href="<?=Url::site('khbackend/reklama/edit/'.$ads_element->id)?>" class="icon-pencil"></a>-->
+				<a href="<?=Url::site('khbackend/reklama/delete/'.$ads_element->id)?>" class="icon-trash delete_article"></a>
+			</td>
+		</tr>
+	<?php endforeach; ?>
 </table>
