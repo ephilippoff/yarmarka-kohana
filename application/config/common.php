38d6b3548c11e4a1a81561419d9a3a23db8736e6
iden <gippon2008@mail.ru>
2014-06-25 13:53:32 +0600

    #yarmarka-48 Установить интервал между поднятиями в 3 дня

--- application/config/common.php
+++ application/config/common.php
@@ -2,7 +2,7 @@
 return array(
 	'main_domain'		=> 'yarmarka.biz',
 	'default_region_id'	=> 73,
-	'days_count_between_service_up' => 7,
+	'days_count_between_service_up' => 3,
 	'disallowed_email_domains' => array('yopmail.com', 'asdasd.com'),
 	'admin_emails' => array('mihail.makeev@gmail.com', 'a.vagapov@yarmarka.biz'),
 );
