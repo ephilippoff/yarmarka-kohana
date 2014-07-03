1e1c2d7d3f492a2074b963d6e8e19c3f857daa29
iden <gippon2008@mail.ru>
2014-07-03 14:03:47 +0600

    #yarmarka-54 Продублировать крошки вверху сайта

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -1,46 +1,61 @@
 <header class="m_header header_image"  <?php if ( ! empty($style)) echo "style='$style'" ?>>
 	<section class="winner">
 		<?php echo View::factory('block/_user_message')?>
-		<div class="m_poll z3">
+		
+	
+		
+		<div class="mt25 z3">
 			<div class="top-line ">
-				<?php if (Auth::instance()->get_user()) : ?>
-						<div class="enter-bl logon iLight"><span class="personl-room iLight-nav"><span>Личный кабинет</span></span>
-							<div class="proom-list iLight-cont">
-
-									<ul>
-										<li><a href="<?=URL::site('user/published')?>"><div class="img"><img src="<?=URL::site('images/pr1.png')?>" alt="" /></div>Мои объявления</a></li>
-										<li><a href="<?=URL::site('user/subscriptions')?>"><div class="img"><img src="" alt="" /></div>Мои подписки</a></li>
-										<li><a href="<?=URL::site('user/favorites')?>"><div class="img"><img src="" alt="" /></div>Мои избранные</a></li>
-										<li><a href="<?=URL::site('user/profile')?>"><div class="img"><img src="<?=URL::site('images/pr2.png')?>" alt="" /></div>Личные данные</a></li>
-										<li><a href="<?=URL::site('user/invoices')?>"><div class="img"><img src="" alt="" /></div>История услуг</a></li>
-										<li><a href="<?=URL::site('user/newspapers')?>"><div class="img"><img src="" alt="" /></div>Купленные газеты</a></li>
-
-										<?php if (Request::current()->action() != 'userpage') : ?>
-											<?php if (Auth::instance()->get_user()->org_type == 2) : ?><li class="last"><a class="green" href="/users/<?=Auth::instance()->get_user()->login?>"><div class="img"><!--<img src="" alt="" />--></div>Страница компании</a></li><?php endif; ?>
-										<?php endif; ?>
-
-										<li class="last"><a href="<?=CI::site('user/logout')?>"><div class="img"><img src="<?=URL::site('images/pr3.png')?>" alt="" /></div>Выход</a></li>
-									</ul>
-
-							</div>
-						</div>
-				<?php endif; ?>
+				<div class="cont cf">
+				<div class="logo">
+					<a href="http://<?=Region::get_current_domain()?>"><img src="<?=URL::site('images/logo.png')?>" alt=""></a>
+					<span class="domen"><?=Region::get_current_domain()?></span>
+				</div>
 				
-				<?=Request::factory('block/header_region')->execute()?>
-				<?php if ($user = Auth::instance()->get_user()) : ?>
-				<div class="who-are-yor"><span>
-					<?=($user->fullname ? $user->fullname : $user->email)?>
-				</span></div>
-				<?php endif; ?>
+				<div class="btn-red big add-advert"><a href="<?=CI::site('add')?>"><span>Подать объявление</span></a></div>				
+												
+				<div class="menu-cont">
+					<!--<a class="lk menu iLight-nav" href="#">Личный кабинет</a>-->
+
+				<?php if (Auth::instance()->get_user()) : ?>	
+
+					<div class="enter-bl logon iLight lk menu">
+						<span class="iLight-nav link cur_p">Личный кабинет</span>
+						<div class="proom-list iLight-cont">
+							<ul>
+								<li><a href="<?=URL::site('user/published')?>"><div class="img"><img src="<?=URL::site("/images/pr1.png")?>" alt="" /></div>Мои объявления</a></li>
+								<li><a href="<?=URL::site('user/subscriptions')?>"><div class="img"><!--<img src="" alt="" />--></div>Мои подписки</a></li>				
+								<li><a href="<?=URL::site('user/favorites')?>"><div class="img"><!--<img src="" alt="" />--></div>Мои избранные</a></li>
+								<li><a href="<?=URL::site('user/profile')?>"><div class="img"><img src="<?=URL::site("/images/pr2.png")?>" alt="" /></div>Личные данные</a></li>
+								<li><a href="<?=URL::site('user/services_history')?>"><div class="img"><!--<img src="" alt="" />--></div>История услуг</a></li>
+								<li><a href="<?=URL::site('user/newspapers')?>"><div class="img"><!--<img src="" alt="" />--></div>Купленные газеты</a></li>
+
+								<?php if (Request::current()->action() != 'userpage') : ?>
+									<?php if (Auth::instance()->get_user()->org_type == 2) : ?><li class="last"><a class="green" href="/users/<?=Auth::instance()->get_user()->login?>"><div class="img"><!--<img src="" alt="" />--></div>Страница компании</a></li><?php endif; ?>
+								<?php endif; ?>
+
+								<li class="last"><a href="<?=CI::site('user/logout')?>"><div class="img"><img src="<?=URL::site('images/pr3.png')?>" alt="" /></div>Выход</a></li>												
+							</ul>
+						</div>
+					</div>	
+
+				<?php endif ?>
+
+					<span onclick="window.location='/article/help'" class="man menu cur_p">Помощь</span>
+
+					<span onclick="window.location='http://feedback.yarmarka.biz/'" class="menu g1 cur_p">Техподдержка</span>
+					<span onclick="window.location='http://job.yarmarka.biz/'" class="menu g1 cur_p">Вакансии</span>
+					<span onclick="window.location='/article/reklama-na-saite-yarmarkabiz'" class="menu g1 cur_p">Реклама на сайте</span>
+					<span onclick="window.location='/article/pravila-razmeshcheniya-obyavlenii'" class="menu g1 cur_p">Правила</span>
+
+					<!--<a class="favorites menu" href="#">Избранное (0)</a>-->
+				</div>				
+					</div>		
 			</div>
-			<div class="logo">
-				<a href="http://<?=Region::get_current_domain()?>">
-					<img src="<?=URL::site('images/logo.png')?>" alt="">
-				</a>
-				<span class="domen"><?=Region::get_current_domain()?></span>
-			</div>
-			<div class="btn-red big add-advert"><a href="<?=CI::site('add')?>"><span>Подать объявление</span></a></div>
+
+
 		</div>
+		
 		<div class="active-bl z2"><div class="active-bl-bg">
 			<div class="m_menu  iLight ">
 				<span  class="choose iLight-nav"></span>
