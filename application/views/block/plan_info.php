fbd9afc5a71c798d3791222f9f7ad40dd0234bcc
almaznv <almaznv@gmail.com>
2014-07-11 16:57:01 +0600

    fix

---
+++ application/views/block/plan_info.php
@@ -0,0 +1,26 @@
+<? if (count($user_plans) == 0) return; ?>
+<div>
+Ваши текущие тарифные планы:<br/>
+<? foreach($user_plans as $plan):?>
+	<? 
+		$current_date = new DateTime();
+		$date_expiration = new DateTime($plan->date_expiration);
+		$interval = date_diff($current_date, $date_expiration);
+		$interval_str = $plan->date_expiration;
+		if ((int) ($interval->format('%d')) < 3)
+			$interval_str = "через ".$interval->format('%d дн. %H:%I:%S')
+	?>
+
+	<?=$plan->plan->title?> на <?=$plan->plan->count?> объявлений, истекает <span><?=$interval_str?></span></br>
+<? endforeach;?>
+</div>
+</br>
+</br>
+<div>
+<? foreach($not_yet_payment as $item):?>
+	В рубрике "<?=$item['title']?>" у вас - <?=$item['count']?> объявлений</br>
+	Ваш текущий план : "<?=$item['current_plan']?>" не более - <?=$item['current_plan_count']?> объявлений.</br>
+	Рекомендуем приобрести тарифный план согласно количества ваших объявлений</br>
+	Объявления сверх лимита будут сняты автоматически.
+<? endforeach;?>
+</div>
