4ca87192d0cd7e33e00cb1283b8af398c442cf62
almaznv <almaznv@gmail.com>
2014-07-23 16:46:05 +0600

    +fix объединялки

--- application/classes/Lib/PlacementAds/AddEdit.php
+++ application/classes/Lib/PlacementAds/AddEdit.php
@@ -179,7 +179,7 @@ class Lib_PlacementAds_AddEdit {
 			if ($object->loaded())
 			{
 				$this->is_edit = TRUE;
-				if ( ! $user->can_edit_object($object->id))
+				if ( ! $user->can_edit_object($object->id) AND !$object->is_union)
 				{
 					$this->raise_error('user can\'t edit this ad');
 				}
