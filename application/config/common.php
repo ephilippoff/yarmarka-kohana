e460b7cd2d29cbdb7e816c92333cbc0c65dd5a58
almaznv <almaznv@gmail.com>
2014-08-06 08:50:01 +0600

    выкинул макеева из админов

--- application/config/common.php
+++ application/config/common.php
@@ -6,7 +6,7 @@ return array(
 	'days_count_between_service_up' 		=> 3,
 	'days_count_between_service_up_by_cat'  => array(36 => 1),
 	'disallowed_email_domains' 				=> array('yopmail.com', 'asdasd.com'),
-	'admin_emails' 							=> array('mihail.makeev@gmail.com', 'a.vagapov@yarmarka.biz'),
+	'admin_emails' 							=> array('a.vagapov@yarmarka.biz'),
 	'max_image_similarity' 					=> 0.97,
 	'check_image_similarity' 				=> FALSE,
 	'check_object_similarity' 				=> TRUE, //вкл выкл функционала проверки объявлений на похожесть (среди своих объяв)
