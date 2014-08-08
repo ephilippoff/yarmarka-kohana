1672286bc78437754b0aeb4585028e78e9fef5e4
almaznv <almaznv@gmail.com>
2014-08-08 16:04:08 +0600

    fix

--- application/classes/Lib/PlacementAds/AddUnion.php
+++ application/classes/Lib/PlacementAds/AddUnion.php
@@ -56,7 +56,6 @@ class Lib_PlacementAds_AddUnion extends Lib_PlacementAds_AddEdit {
 		$objects_for_union = &$this->objects_for_union;
 
 		$exist_photo = Array();
-		DB::delete("object_attachment")->where('object_id', 'IN', array_values($objects_for_union))->execute();
 		$data = ORM::factory('Object_Attachment')->where('object_id', '=', $object->id)->find_all();
 		foreach ($data as $item)
 			$exist_photo[] = $item->signature;
@@ -96,7 +95,6 @@ class Lib_PlacementAds_AddUnion extends Lib_PlacementAds_AddEdit {
 		$objects_for_union = &$this->objects_for_union;
 
 		$exist_values = Array();
-		DB::delete("data_list")->where('object', 'IN', array_values($objects_for_union))->execute();
 		$data = ORM::factory('Data_List')->where('object', '=', $object->id)->find_all();
 		foreach ($data as $item)
 			$exist_values[] = $item->value;
@@ -126,7 +124,6 @@ class Lib_PlacementAds_AddUnion extends Lib_PlacementAds_AddEdit {
 
 
 		$exist_values = Array();
-		DB::delete("data_integer")->where('object', 'IN', array_values($objects_for_union))->execute();
 		$data = ORM::factory('Data_Integer')->where('object', '=', $object->id)->find_all();
 		foreach ($data as $item)
 			$exist_values[] = Array($item->reference ,$item->value_min, $item->value_max);		
@@ -157,7 +154,6 @@ class Lib_PlacementAds_AddUnion extends Lib_PlacementAds_AddEdit {
 
 
 		$exist_values = Array();
-		DB::delete("data_numeric")->where('object', 'IN', array_values($objects_for_union))->execute();
 		$data = ORM::factory('Data_Numeric')->where('object', '=', $object->id)->find_all();
 		foreach ($data as $item)
 			$exist_values[] = Array($item->reference, $item->value_min);		
