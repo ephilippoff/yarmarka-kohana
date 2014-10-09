091d48df66375bb859b3f931417fe2e72f52c6d1
almaznv <almaznv@gmail.com>
2014-10-09 09:57:01 +0600

    все загрузки, даже на сто процентов с ошибками втсают на модерацию

--- application/config/massload.php
+++ application/config/massload.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 return array(
 	'up_percent' => 10,
-	'allow_error_percent' => 70,
+	'allow_error_percent' => 100,
 	'free_limit' => 30,
 	'frontend_load_category' => array("flat_resale", "house", "land")
 );
