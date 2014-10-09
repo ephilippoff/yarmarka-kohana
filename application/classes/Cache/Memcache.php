9285ef0f4dd84f3d7f2fad8469397a82f98cf5b8
almaznv <almaznv@gmail.com>
2014-10-09 11:31:53 +0600

    разделил мемкеш для девелоперсикх ресусров и боевого

--- application/classes/Cache/Memcache.php
+++ application/classes/Cache/Memcache.php
@@ -2,9 +2,18 @@
 
 class Cache_Memcache extends Kohana_Cache_Memcache {
 
+	private $env = "";
+
+	function __construct(array $config)
+	{
+		if (Kohana::$environment == Kohana::DEVELOPMENT)
+			$this->env = "dev_";
+		parent::__construct($config);
+	}
 
 	public function set($id, $data, $lifetime = 3600, $tag = NULL)
 	{
+		$id = $this->env.$id;
 		if ($tag AND $lifetime>3600)
 		{
 			$memcached = ORM::factory('Memcached');
@@ -19,6 +28,30 @@ class Cache_Memcache extends Kohana_Cache_Memcache {
 		}
 
 		parent::set($id, $data, $lifetime);
+	}
+
+	public function get($id, $default = NULL)
+	{
+		$id = $this->env.$id;
+		parent::get($id, $default);
+	}
+
+	public function delete($id, $timeout = 0)
+	{
+		$id = $this->env.$id;
+		parent::delete($id, $timeout);
+	}
+
+	public function increment($id, $step = 1)
+	{
+		$id = $this->env.$id;
+		parent::increment($id, $step);
+	}
+
+	public function decrement($id, $step = 1)
+	{
+		$id = $this->env.$id;
+		parent::decrement($id, $step);
 	}
 
 }
