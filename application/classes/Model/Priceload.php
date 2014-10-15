6c76b6b1f8a21ae2e08e0328d986e866c99a54b8
almaznv <almaznv@gmail.com>
2014-10-15 23:39:28 +0600

    table priceload

--- application/classes/Model/Priceload.php
+++ application/classes/Model/Priceload.php
@@ -9,4 +9,38 @@ class Model_Priceload extends ORM {
 		'user'			=> array('model' => 'User', 'foreign_key' => 'user_id'),
 	);
 
+	/*
+		-- Table: priceload
+
+		-- DROP TABLE priceload;
+
+		CREATE TABLE priceload
+		(
+		  id serial NOT NULL,
+		  user_id integer,
+		  created_on timestamp without time zone DEFAULT now(),
+		  title character varying(255),
+		  filepath character varying(255),
+		  table_name character varying(255),
+		  config text,
+		  statistic character varying(255),
+		  state integer DEFAULT 0, -- 0 - default...
+		  comment text,
+		  CONSTRAINT priceload_pkey PRIMARY KEY (id)
+		)
+		WITH (
+		  OIDS=FALSE
+		);
+		ALTER TABLE priceload
+		  OWNER TO yarmarka_biz;
+		COMMENT ON COLUMN priceload.state IS '0 - default
+		1 - on_moderation
+		2 - true_moderation
+		3 - false_moderation
+		4 - in order 
+		5 - finished';
+
+
+	 */
+
 }
