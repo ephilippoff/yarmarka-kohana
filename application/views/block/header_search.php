06dd7c6cc5f1ce3a1f6860a433ea024e24c344c6
iden <gippon2008@mail.ru>
2014-11-12 11:22:28 +0500

    Категории из поиск. панели убраны

--- application/views/block/header_search.php
+++ application/views/block/header_search.php
@@ -5,13 +5,6 @@
 <!--noindex-->
 <div class="seach-bl-fix">
 	<div class="seach-bl">
-		<select class="cusel-plagin" name="" id="sd1" onChange="$('#search-form').attr('action', this.value)">
-			<option value="search">Все категории</option>
-			<option class="line" value=""></option>
-			<?php foreach ($categories as $category) : ?>
-			<option value="<?=$category->get_url()?>"><?=$category->title?></option>
-			<?php endforeach; ?>
-		</select>
 		<div class="input-seach"><input type="text" name="k"></div>
 	</div>
 </div>
