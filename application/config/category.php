36b675a876f0e0f315e9c1f3a8b83d0e6332dc74
almaznv <almaznv@gmail.com>
2014-11-17 11:08:27 +0500

    расширил пеерчень рубрик в которые можно загрузить прайс

--- application/config/category.php
+++ application/config/category.php
@@ -1,6 +1,36 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 return array(
+	"23" => array( //автозапчасти
+		"price_enabled" => TRUE
+	),
+	"26" => array( //аренда авто
+		"price_enabled" => TRUE
+	),
+	"29" => array( //грузопассажирские перевозки
+		"one_mobile_phone" => TRUE,
+		"max_count" => 1,
+		"description" => "one_advert_category",
+		"price_enabled" => TRUE
+	),
+	"36" => array( //вакансии
+		"description" => "vakancy_law"
+	),
+	"41" => array( //обучение курсы
+		"price_enabled" => TRUE
+	),
+	"42" => array( //медицина и здоровье
+		"price_enabled" => TRUE
+	),
+	"43" => array( //Юридические услуги
+		"price_enabled" => TRUE
+	),
+	"44" => array( //финансы и аудит
+		"price_enabled" => TRUE
+	),
+	"45" => array( //красота и здоровье
+		"price_enabled" => TRUE
+	),
 	"49" => array(//"Ремонт и обслуживание бытовой техники"
 		"one_mobile_phone" => TRUE,
 		"max_count" => 1,
@@ -19,10 +49,16 @@ return array(
 		"description" => "one_advert_category",
 		"price_enabled" => TRUE
 	),
+	"52" => array( //установка и изготовление на заказ
+		"price_enabled" => TRUE
+	),
 	"53" => array(//"Услуги сантехника"
 		"one_mobile_phone" => TRUE,
 		"max_count" => 1,
 		"description" => "one_advert_category",
+		"price_enabled" => TRUE
+	),
+	"54" => array( //строиельные работы
 		"price_enabled" => TRUE
 	),
 	"56" => array(//"Услуги электрика"
@@ -37,25 +73,49 @@ return array(
 		"description" => "one_advert_category",
 		"price_enabled" => TRUE
 	),
+	"58" => array( //инструменты и инвентарь
+		"price_enabled" => TRUE
+	),
+	"59" => array( //оборудование для бизнеса
+		"price_enabled" => TRUE
+	),
+	"60" => array( //мебель и интерьер
+		"price_enabled" => TRUE
+	),
+	"75" => array( //Другие предмееты потребелния
+		"price_enabled" => TRUE
+	),
+	"113" => array( //велосипеды
+		"price_enabled" => TRUE
+	),
+	"126" => array( //строит и отделочные материалы
+		"price_enabled" => TRUE
+	),
+	"133" => array( //репетиторство
+		"price_enabled" => TRUE
+	),
+	"134" => array( //бизнес образование
+		"price_enabled" => TRUE
+	),	
+	"139" => array( //водный транспорт
+		"price_enabled" => TRUE
+	),
+	"142" => array( //малый коммерческий транспорт
+		"price_enabled" => TRUE
+	),
+	"143" => array( //Спецтехника
+		"price_enabled" => TRUE
+	),
+	"146" => array( //Тракторы
+		"price_enabled" => TRUE
+	),
 	"154" => array(//"Ремонт и обслуживание оргтехники"
 		"one_mobile_phone" => TRUE,
 		"max_count" => 1,
 		"description" => "one_advert_category",
 		"price_enabled" => TRUE
 	),
-	"29" => array( //грузопассажирские перевозки
-		"one_mobile_phone" => TRUE,
-		"max_count" => 1,
-		"description" => "one_advert_category",
-		"price_enabled" => TRUE
-	),
-	"36" => array( //вакансии
-		"description" => "vakancy_law"
-	),
 	"155" => array( //каталог компаний
-		"price_enabled" => TRUE
-	),
-	"23" => array( //автозапчасти
 		"price_enabled" => TRUE
 	)
 );
