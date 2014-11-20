e86854f1ad2a9d9a23d1fb0a3ff686a464b07924
almaznv <almaznv@gmail.com>
2014-11-20 09:38:47 +0500

    +авто рубрику в массовые загрузки

--- application/config/massload/avito_categories.php
+++ application/config/massload/avito_categories.php
@@ -1,5 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 return array(
+	'auto'				=> array( "Category" => "Автомобили с пробегом"),
 	'flat_resale'		=> array( "Category" => "Квартиры", "MarketType" => "Вторичка", "OperationType" => "Продам"),
 	'flat_rooms'		=> array( "Category" => "Комнаты",  "OperationType" => "Продам"),
 	'flat_new'			=> array( "Category" => "Квартиры", "MarketType" => "Новостройка", "OperationType" => "Продам"),
