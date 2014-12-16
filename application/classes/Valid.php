59ed7af227cc461f41ab8536277fac2721db2cfa
almaznv <almaznv@gmail.com>
2014-12-16 12:55:43 +0500

    fix valid numeric values

--- application/classes/Valid.php
+++ application/classes/Valid.php
@@ -88,7 +88,7 @@ class Valid extends Kohana_Valid {
 
 	public static function min_value($value, $title, $min_value = 0)
 	{
-		return intval($value) > $min_value;
+		return $value > $min_value;
 	}
 
 	public static function max_value($value, $title, $max_value = 999999999)
