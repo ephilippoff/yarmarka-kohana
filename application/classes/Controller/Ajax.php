a8a2e674a77398e006f524867302949b6dfeb40b
iden <gippon2008@mail.ru>
2014-12-19 09:18:59 +0500

    Включен контроль ajax-запроса

--- application/classes/Controller/Ajax.php
+++ application/classes/Controller/Ajax.php
@@ -19,7 +19,7 @@ class Controller_Ajax extends Controller_Template
 		// only ajax request is allowed
 		if ( ! $this->request->is_ajax() AND Kohana::$environment !== Kohana::DEVELOPMENT)
 		{
-//			throw new HTTP_Exception_404;
+			throw new HTTP_Exception_404;
 		}
 		// disable global layout for this controller
 		$this->use_layout = FALSE;
