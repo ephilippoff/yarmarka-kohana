1df4b7a04a060d98b60cd23be14c179239240883
almaznv <almaznv@gmail.com>
2014-12-24 16:43:38 +0500

    интерфейс для логина произвольного пользователя для админов

--- application/views/admin/settings/index.php
+++ application/views/admin/settings/index.php
@@ -1,1 +1,9 @@
 <h2>Настройки</h2>
+
+<form class="form-inline" method="post" action="">
+	<div class="input-prepend">
+		<span class="add-on"><i class="icon-envelope"></i></span>
+		<input class="span2" id="prependedInput" type="text" placeholder="User email" name="email" value="<?=Arr::get($_GET, 'email')?>">
+    </div>
+    <input type="submit" name="" value="Enter" class="btn btn-primary">
+</form>
