c76dc9761e0c931d1c2c5a31ffe676cbef1c5b11
iden <gippon2008@mail.ru>
2014-12-25 13:51:13 +0500

    #yarmarka-165 Создать рассылку письма клиентам

---
+++ application/classes/Task/Mailsender.php
@@ -0,0 +1,18 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Task_Mailsender extends Minion_Task
+{
+	protected function _execute(array $params)
+	{		
+		//$users = ORM::factory('User')->where('is_blocked', '=', 0)->find_all()->as_array('id', 'email');
+		//check mail
+		$users = ORM::factory('User')->where('email', '=', 'd.Istomin@yarmarka.biz')->find_all()->as_array('id', 'email');
+		
+		foreach ($users as $email) 
+		{
+			$msg = View::factory('emails/newyear')->render();			
+			Email::send(trim($user->email), Kohana::$config->load('email.default_from'), "Поздравляем с новым годом!", $msg);
+		}				
+	}
+
+}
---
+++ application/views/emails/newyear.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang=ru>
+<head>
+<meta charset=utf-8>
+<title></title>
+
+</head>
+
+<body  align="center">	
+	<table style="width:100%;">
+		<tr>
+			<td align="center">
+				<img src="http://yarmarka.biz/images/ny2015.jpg">		
+			</td>
+		</tr>
+	</table>	
+</body>  
+</html>
