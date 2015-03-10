941240bf6dd057277f43160425a4e10ec5cae535
almaznv <almaznv@gmail.com>
2015-03-10 16:20:40 +0500

    fix

--- application/classes/Imageci.php
+++ application/classes/Imageci.php
@@ -600,7 +600,7 @@ class Imageci {
 			if ($size == "original")  $size = "orig";
 			if (file_exists(".".$path))
 			{
-				$destination = ".".str_replace($size, "$dest/$size", $path);
+				$destination = str_replace("uploads/".$size, "/backup/uploads/$dest/$size", $path);
 				//Minion_CLI::write("moved:".$path." - ".$destination);
 				if (!is_dir(dirname($destination))) {
 					mkdir(dirname($destination), 0777, true);
