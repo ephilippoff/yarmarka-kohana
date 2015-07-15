013c45edb2c82dcd5dcc5f3ce1a4b7498bcfc6a3
almaznv <almaznv@gmail.com>
2015-07-15 09:54:56 +0500

    fix

--- application/config/seo.php
+++ application/config/seo.php
@@ -1,6 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 return array(
 	'noindex_routes'	=> array(
+		"add","user","admin", "backend", "cart", "userpage", "object_edit"
 	),
 	'title_postfix'		=> ' - Газета бесплатных объявлений Ярмарка',
 );
