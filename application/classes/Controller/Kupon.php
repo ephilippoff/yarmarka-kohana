190e2e2d917e8c324f4212089f53e3c926ce4c23
almaznv <almaznv@gmail.com>
2015-09-28 15:43:18 +0500

    генерация номера при покупке, шифрация номера

--- application/classes/Controller/Kupon.php
+++ application/classes/Controller/Kupon.php
@@ -32,7 +32,11 @@ class Controller_Kupon extends Controller_Template {
 			throw new HTTP_Exception_404;
 		
 		$twig->kupon = $kupon;
+		$twig->kupon_number = $kupon->decrypt_number($twig->kupon->number);
 		$twig->kupon_group = $kupon_group;
+
+		echo Debug::vars($kupon->check_number("367651664"));
+
 		$object = ORM::factory('Object', $kupon_group->object_id);
 		if ($object->loaded())
 		{
