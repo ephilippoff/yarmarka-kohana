21854203bcdf4b47a6672542d68445160151a800
iden <gippon2008@mail.ru>
2015-10-01 16:48:19 +0500

    Правки

--- application/views/article/ourservices/_menu_gazeta-sg.php
+++ application/views/article/ourservices/_menu_gazeta-sg.php
@@ -1,6 +1,5 @@
 						<ul class="right-menu fn-right-menu">
 							<li><a href="/ourservices/specialnie-predlozheniia-sg#item1">Выгодные предложения</a></li>
 							<li><a href="/ourservices/specialnie-predlozheniia-sg#item2">Премиум размещение</a></li>
-							<li><a href="/ourservices/specialnie-predlozheniia-sg#item3">Рубрика &laquo;Развлечения и отдых&raquo;</a></li>
 							<li><a href="/ourservices/specialnie-predlozheniia-sg#item4">Сквозное размещение</a></li>
 						</ul>
