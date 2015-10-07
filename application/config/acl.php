197a09c7d95c2c25298c46d262bba4aafe92df07
iden <gippon2008@mail.ru>
2015-10-07 16:09:56 +0500

    config acl edit

--- application/config/acl.php
+++ application/config/acl.php
@@ -10,7 +10,7 @@ return array(
 			"edit" => array(1,3,9, "auth", "owner"),
 			"moderate" => array(1,3,9, "auth"),
 			"add" => array(
-				"type" => array(1, 9, "auth")
+				"type" => array(1,3,9, "auth")
 			)
 		),
 		"pay_service" => array("auth", 1),
