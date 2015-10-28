4c911e4b77fb5d136305a156c47704b4df1a800e
almaznv <almaznv@gmail.com>
2015-10-28 11:47:45 +0500

    fix

--- application/views/layouts/admin_head.php
+++ application/views/layouts/admin_head.php
@@ -1,4 +1,4 @@
-<?=HTML::script('js/jquery-1.9.1.js')?>
-<?=HTML::script('js/adaptive/underscore.js')?>
+<?=HTML::script('static/develop/js/lib/jquery.js')?>
+<?=HTML::script('static/develop/js/lib/underscore.js')?>
 <?=HTML::script('bootstrap/js/bootstrap.min.js')?>
 <?=HTML::script('bootstrap/admin.js')?>
