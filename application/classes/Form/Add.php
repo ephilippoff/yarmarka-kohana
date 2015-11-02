8b689cb9981b22981ae430b217b0b0fc63008c80
almaznv <almaznv@gmail.com>
2015-11-02 11:18:21 +0500

    fix edit

--- application/classes/Form/Add.php
+++ application/classes/Form/Add.php
@@ -684,9 +684,16 @@ class Form_Add  {
 		$main_image_file = "";
 		if ($main_image_id) {
 
-			$oa = ORM::factory('Object_Attachment')
-					->where("filename","=",$main_image_id)
-					->find();
+			if (is_int($main_image_id)) {
+				$oa = ORM::factory('Object_Attachment')
+						->where("id","=",$main_image_id)
+						->find();
+			} else {
+				$oa = ORM::factory('Object_Attachment')
+						->where("filename","=",$main_image_id)
+						->find();
+			}
+			
 			if ($oa->loaded()) {
 				$main_image_file = $oa->filename;
 			}
