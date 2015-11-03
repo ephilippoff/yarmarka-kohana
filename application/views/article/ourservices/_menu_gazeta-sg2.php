1c6523916fc614a364064d2862649f79a08e3884
iden <gippon2008@mail.ru>
2015-11-03 17:07:59 +0500

    Убрать описание премиум Сг

--- application/views/article/ourservices/_menu_gazeta-sg2.php
+++ application/views/article/ourservices/_menu_gazeta-sg2.php
@@ -1,5 +1,4 @@
 						<ul class="right-menu fn-right-menu">
 							<li><a href="#item1">Выгодные предложения</a></li>
-							<li><a href="#item2">Премиум размещение</a></li>
 							<li><a href="#item4">Сквозное размещение</a></li>
 						</ul>
