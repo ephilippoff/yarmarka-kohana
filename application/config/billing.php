5f0ec3ae0974454ac0579e1502717796fcf9c42c
iden <gippon2008@mail.ru>
2015-11-23 16:45:55 +0500

    email

--- application/config/billing.php
+++ application/config/billing.php
@@ -1,8 +1,8 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 return array(
 	'emails_for_notify' => array("a.vagapov@yarmarka.biz",'v.kurkin@yarmarka.biz','d.istomin@yarmarka.biz','e.philippov@yarmarka.biz'),
-	'operators_for_notify' => array('a.vagapov@yarmarka.biz','tsrobottmn@tmn.yarmarka.biz','v.kurkin@yarmarka.biz'),
-	'managers_for_notify' => array("a.vagapov@yarmarka.biz",'v.kurkin@yarmarka.biz','d.istomin@yarmarka.biz'),
+	'operators_for_notify' => array('a.vagapov@yarmarka.biz','tsrobottmn@tmn.yarmarka.biz','v.kurkin@yarmarka.biz','e.philippov@yarmarka.biz'),
+	'managers_for_notify' => array("a.vagapov@yarmarka.biz",'v.kurkin@yarmarka.biz','d.istomin@yarmarka.biz','e.philippov@yarmarka.biz'),
 	'kupon' => array(
 		'delivery_type' => 'electronic'
 	)
