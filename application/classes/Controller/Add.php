b1acb6769ab75ff47a8d8d49bbb6c8b3e247bf91
almaz vagapov <almaznv@gmail.com>
2015-12-12 12:32:43 +0300

    fix

--- application/classes/Controller/Add.php
+++ application/classes/Controller/Add.php
@@ -246,11 +246,15 @@ class Controller_Add extends Controller_Template {
 		}
 
 
-		$twig->params->moder_messages = ORM::factory('User_Messages')
-		                        ->get_messages_from_admins($object_id)
-		                        ->order_by("createdOn", "desc")
-		                        ->limit(3)
-		                        ->getprepared_all();
+		if ($object->is_bad) {
+			$twig->params->moder_messages = ORM::factory('User_Messages')
+			                        ->get_messages_from_admins($object_id)
+			                        ->order_by("createdOn", "desc")
+			                        ->limit(3)
+			                        ->getprepared_all();
+		} else {
+			$twig->params->moder_messages = array();
+		}
 
 		$twig->params->expired_orginfo = $expired;
 		$twig->params = (array) $twig->params;
