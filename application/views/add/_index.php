8d6f1c52ea7047a5e4e3d8d2ce91349b8fce6efe
qwetexac <qwetexac@gmail.com>
2015-12-15 10:29:27 +0300

    Последние правки

--- application/views/add/_index.php
+++ application/views/add/_index.php
@@ -13,6 +13,11 @@
 		<input type="hidden" name="session_id" value="<?= session_id() ?>">
 
 		<? if (property_exists($form_data, 'login')): ?>
+			<div class="row">
+				<div class="col-lg-push-3 col-lg-9 col-md-push-3 col-md-9 mb20">
+					Представьтесь, пожалуйста:
+				</div>
+			</div>
 			<div class="row mb10">
 				<div class="col-md-3 col-xs-12 labelcont">
 					<label>Логин:</label>
@@ -184,6 +189,85 @@
 			<hr class="dib mb30 w100p">
 		<? endif; ?>
 
+		<? if (property_exists($form_data, 'contacts')): ?>
+			<div class="row">
+				<div class="col-md-3">&nbsp;</div>
+				<div class="col-md-9 mb20">
+				! Внимательно заполните контактные данные. Ввод телефона следует начинать с кода оператора (для мобильного телефона), или с кода города.
+				</div>
+			</div>
+			<div class="row mb10">
+				<div class="col-md-3 col-xs-12 labelcont">
+					<label>Контактное лицо:</label>
+				</div>
+				<div class="col-md-9 col-xs-12">
+					<div class="row"  data-type="email">
+						<div class="col-md-6">
+							<div id="contacts">	                  			
+								<div class="inp-cont <? if ($form_data->contacts["contact_error"]) echo "error"; ?>">
+									<span class="required-star">*</span>						
+									<input class="w100p" type="text" name="contact" value="<?= $form_data->contacts["contact_person"] ?>"/>						
+									<? if ($form_data->contacts AND $form_data->contacts["contact_error"]): ?>
+										<span class="inform">
+											<?= $form_data->contacts["contact_error"] ?>
+										</span>
+									<? endif; ?>
+								</div>
+							</div>
+						</div>
+					</div>				
+				</div>
+			</div>	
+			<? foreach (array(
+					"mobile" => array("name"=>"Мобильный телефон", "icon" => "<i class='fa fa-mobile-phone'></i>", "placeholder" => "Введите код из смс"),
+					"phone" => array("name"=>"Городской телефон", "icon" => "<i class='fa fa-phone'></i>", "placeholder" => "Введите код"),
+					"email"=> array("name"=>"Email", "icon" => "<i class='fa fa-envelope-o'></i>", "placeholder" => "Введите код из письма")) as $type => $type_params): ?>
+
+				<div class="row mb20">
+					<div class="col-md-3 labelcont">
+						<label><?=$type_params["name"]?>:</label>
+					</div>
+					<div class="col-md-9">
+						<div class="row js-contact" data-type="<?=$type?>">
+							<div class="col-md-8 inp-cont <? if ($form_data->contacts["contact_".$type."_error"]) echo "error"; ?>">
+								<?
+									$contact = (isset($form_data->contacts['contacts'][$type])) ? new Obj($form_data->contacts['contacts'][$type]) : new Obj();
+								?>
+								<div class="input-group w100p">
+									<span class="input-group-addon bg-color-whitesmoke brl3" title="<?=$type_params["name"]?>"><?=$type_params["icon"]?></span>
+									<input class="form-control js-contact-value" type="text" name="contact_<?=$type?>" value="<?=$contact->value?>">
+									<? if ($contact->value): ?>
+										<? if ($contact->verified): ?>
+											<span class="input-group-addon button bg-color-lightgreen white pl5 pr5 brr3 js-contact-ok"><i class="fa fa-check"></i></span>
+										<? else: ?>
+											<span class="input-group-addon button bg-color-crimson white pl5 pr5 brr3 js-contact-ok" data-state="ready">Нажмите чтобы подтвердить</span>
+										<? endif; ?>
+									<? else: ?>
+										<span class="input-group-addon button bg-color-gray white pl5 pr5 brr3 js-contact-ok"><i class="fa fa-question"></i></span>
+									<? endif; ?>
+								</div>
+								<? if ($form_data->contacts AND $form_data->contacts["contact_".$type."_error"]): ?>
+									<span class="inform">
+										<?= $form_data->contacts["contact_".$type."_error"] ?>
+									</span>
+								<? endif; ?>
+							</div>
+							<div class="col-md-4 inp-cont error">
+								<div class="input-group w100p hidden js-contact-code">
+									<input class="form-control w100 js-contact-code-value" type="text" placeholder="<?=$type_params["placeholder"]?>">
+									<span class="input-group-addon button bg-color-crimson white pl5 pr5 brr3 js-contact-code-ok">ок</span>
+								</div>
+								<span class="inform hidden js-contact-code-description"></span>
+								<div class="input-group w100p hidden js-contact-description"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<? endforeach; ?>	
+			<hr class="dib mb30 w100p">
+			
+		<? endif; ?>
 		<? if (property_exists($form_data, 'city')): ?>
 			<div id="div_city">
 				<div class="row mb10">
@@ -513,7 +597,8 @@
 							</div>
 						</div>				
 					</div>
-				</div>			
+				</div>		
+				<hr class="dib mb30 w100p">	
 			<? endif; ?>
 		</div>
 
@@ -543,94 +628,13 @@
 			<? endif; ?>
 		</div>
 
-		<? if (property_exists($form_data, 'contacts')): ?>
-			<hr class="dib mb30 w100p">
-			<div class="row">
-				<div class="col-md-3">&nbsp;</div>
-				<div class="col-md-9 mb20">
-				! Внимательно заполните контактные данные. Ввод телефона следует начинать с кода оператора (для мобильного телефона), или с кода города.
-				</div>
-			</div>
-			<? foreach (array(
-					"mobile" => array("name"=>"Мобильный телефон", "icon" => "<i class='fa fa-mobile-phone'></i>", "placeholder" => "Введите код из смс"),
-					"phone" => array("name"=>"Городской телефон", "icon" => "<i class='fa fa-phone'></i>", "placeholder" => "Введите код"),
-					"email"=> array("name"=>"Email", "icon" => "<i class='fa fa-envelope-o'></i>", "placeholder" => "Введите код из письма")) as $type => $type_params): ?>
-
-				<div class="row mb20">
-					<div class="col-md-3 labelcont">
-						<label><?=$type_params["name"]?>:</label>
-					</div>
-					<div class="col-md-9">
-						<div class="row js-contact" data-type="<?=$type?>">
-							<div class="col-md-8 inp-cont <? if ($form_data->contacts["contact_".$type."_error"]) echo "error"; ?>">
-								<?
-									$contact = (isset($form_data->contacts['contacts'][$type])) ? new Obj($form_data->contacts['contacts'][$type]) : new Obj();
-								?>
-								<div class="input-group w100p">
-									<span class="input-group-addon bg-color-whitesmoke brl3" title="<?=$type_params["name"]?>"><?=$type_params["icon"]?></span>
-									<input class="form-control js-contact-value" type="text" name="contact_<?=$type?>" value="<?=$contact->value?>">
-									<? if ($contact->value): ?>
-										<? if ($contact->verified): ?>
-											<span class="input-group-addon button bg-color-lightgreen white pl5 pr5 brr3 js-contact-ok"><i class="fa fa-check"></i></span>
-										<? else: ?>
-											<span class="input-group-addon button bg-color-crimson white pl5 pr5 brr3 js-contact-ok" data-state="ready">Нажмите чтобы подтвердить</span>
-										<? endif; ?>
-									<? else: ?>
-										<span class="input-group-addon button bg-color-gray white pl5 pr5 brr3 js-contact-ok"><i class="fa fa-question"></i></span>
-									<? endif; ?>
-								</div>
-								<? if ($form_data->contacts AND $form_data->contacts["contact_".$type."_error"]): ?>
-									<span class="inform">
-										<?= $form_data->contacts["contact_".$type."_error"] ?>
-									</span>
-								<? endif; ?>
-							</div>
-							<div class="col-md-4 inp-cont error">
-								<div class="input-group w100p hidden js-contact-code">
-									<input class="form-control w100 js-contact-code-value" type="text" placeholder="<?=$type_params["placeholder"]?>">
-									<span class="input-group-addon button bg-color-crimson white pl5 pr5 brr3 js-contact-code-ok">ок</span>
-								</div>
-								<span class="inform hidden js-contact-code-description"></span>
-								<div class="input-group w100p hidden js-contact-description"></div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-			<? endforeach; ?>
-
-			<div class="row mb10">
-				<div class="col-md-3 col-xs-12 labelcont">
-					<label>Контактное лицо:</label>
-				</div>
-				<div class="col-md-9 col-xs-12">
-					<div class="row"  data-type="email">
-						<div class="col-md-6">
-							<div id="contacts">	                  			
-								<div class="inp-cont <? if ($form_data->contacts["contact_error"]) echo "error"; ?>">
-									<span class="required-star">*</span>						
-									<input class="w100p" type="text" name="contact" value="<?= $form_data->contacts["contact_person"] ?>"/>						
-									<? if ($form_data->contacts AND $form_data->contacts["contact_error"]): ?>
-										<span class="inform">
-											<?= $form_data->contacts["contact_error"] ?>
-										</span>
-									<? endif; ?>
-								</div>
-							</div>
-						</div>
-					</div>				
-				</div>
-			</div>		
-
-			<hr class="dib mb30 w100p">
-		<? endif; ?>
-
 
 		<?
 		$prolongation_access = FALSE;
 		?>  
 
 		<? if ($form_data->_edit): ?>
+		<hr class="dib mb30 w100p">
 			<div class="row mb10">
 				<div class="col-md-3 col-xs-12 labelcont">
 					<label>Состояние объявления:</label>
