29cc1b61092d856b3a9095dfdf5d633ef2b7120c
almaz vagapov <almaznv@gmail.com>
2016-01-24 19:39:25 +0300

    fix connecting accounts

--- application/views/user/userinfo.php
+++ application/views/user/userinfo.php
@@ -89,8 +89,8 @@
 
 								function isValidEmail(email)
 								{
-								    return /^[a-z0-9]+([-._][a-z0-9]+)*@([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,4}$/.test(email)
-								        && /^(?=.{1,64}@.{4,64}$)(?=.{6,100}$).*/.test(email);
+								    var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+								    return regex.test(email);
 								}
 
 								function request_to_company()
