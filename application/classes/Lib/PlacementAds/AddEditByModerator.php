607b6dea443f54aab8ee24a94b98cac225e9eb1d
almaz vagapov <almaznv@gmail.com>
2016-01-24 20:32:11 +0300

    fix yarmarka-501

--- application/classes/Lib/PlacementAds/AddEditByModerator.php
+++ application/classes/Lib/PlacementAds/AddEditByModerator.php
@@ -131,4 +131,67 @@ class Lib_PlacementAds_AddEditByModerator extends Lib_PlacementAds_AddEdit {
 		return $this;		
 	}
 
+	function init_additional()
+	{
+		$category = &$this->category;
+		$validation = &$this->validation;
+		$params = &$this->params;
+
+		if ($category AND $settings = Kohana::$config->load("category.".$category->id.".additional_fields.2"))
+		{
+
+			$titles =  Kohana::$config->load("dictionaries.additional_fields.2");
+			foreach ($settings as $setting) {
+				$validation->rules($setting, array(
+						array('not_empty', array(':value', $titles[$setting]))
+					)
+				);
+			}
+		}
+
+		if ($category AND $saveas = Kohana::$config->load("category.".$category->id.".additional_saveas"))
+		{
+			if (!$saveas)
+				$saveas = array();
+
+			foreach ($saveas as $field => $_saveas) {
+
+					$param = $saveas[$field][0];
+					$value = trim($params->{$field});
+					if ($value)
+						$params->{$param} = $value;
+					else
+						$params->{$param} = $saveas[$field][1];
+			}
+		}
+		return $this;
+	}
+
+	function save_additional()
+	{
+		$params = &$this->params;
+		$object = &$this->object;
+
+		if ($object->category AND $settings = Kohana::$config->load("category.".$object->category.".additional_fields.2"))
+		{
+			foreach ($settings as $setting) {
+				$name = str_replace("additional_", "", $setting);
+				$value = $params->{$setting};
+
+				if ($this->is_edit AND $this->user_id) {
+
+					if (!$value)
+						ORM::factory('User_Settings')
+							->_delete($this->user_id, "orginfo", $name);
+					
+					ORM::factory('User_Settings')
+							->update_or_save($this->user_id, "orginfo", $name, $value);
+
+				}
+			}
+		}
+
+		return $this;
+	}
+
 }
