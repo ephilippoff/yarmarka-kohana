b46c04e9938ec7ed6fc8c2a21da79d99e8608cdc
qwetexac <qwetexac@gmail.com>
2016-02-16 20:22:37 +0300

    email adaptive

--- application/views/emails/manage_object.php
+++ application/views/emails/manage_object.php
@@ -6,56 +6,37 @@
 
 </head>
 
-<body  align="center">
+<body style="font-family: Verdana, Geneva, Arial, Helvetica, sans-serif; font-size: 14px; margin: 0; padding: 0;">
 
-<table align="center" cellspacing="0" cellpadding="0" border="0" width="800" style="color:#000;font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;font-size: 14px;border-collapse: collapse;">
-   
-        <tbody style="color:#000;font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;font-size: 14px;">
-   
-		<tr style="color:#000;font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;font-size: 14px;background:#ECF3F7;">
-			<td width="280" style="padding: 10px 0 0 10px;"><img width="172" height="33" src="<?=URL::site('static/develop/production/images/logo.png', 'http')?>" alt="Ярмарка" align="left" /></td>
-			<td style="text-align: right;vertical-align: bottom;padding: 10px 10px 0px 0px;color:#616161;font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;font-size: 14px;"><span style="color:#D44234;font-size:24px;">yarmarka</span><span style="color:#5B6772;font-size:24px;">.biz</span></td>
-		</tr>
-		<tr style="color:#000;font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;font-size: 10px;background:#ECF3F7;">
-			<td width="280" style="text-align: left;padding: 0px 0px 10px 10px;font-size: 11px;">сайт бесплатных объявлений</td>
-			<td style="text-align: right;vertical-align: bottom;line-height: 2.6;color:#616161;font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;font-size: 14px;"></td>
-		</tr>
-			
-		<tr><td colspan="2">&nbsp;</td></tr>   
-   
-		<tr>
-			<td width="280"><img src="<?=URL::site('static/develop/production/images/9_165.jpg', 'http')?>" /></td>
-			<td>
-				<table align="center" cellspacing="0" cellpadding="0" border="0" width="" style="color:#000;font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;border-collapse: collapse;">
-					<tbody>	
-						<tr><td align="right" colspan="2" style="font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;line-height: 12px;"><a style="color:#fff;padding:5px 10px;background-color:#D44234;text-decoration:none;" href="http://c.yarmarka.biz/add">Подать объявление</a></td></tr>
-						<tr><td colspan="2">&nbsp;</td></tr>
-						<tr><td align="center" colspan="2" style="font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;line-height: 15px;font-size:18px;font-weight:bold;">Здравствуйте!</td></tr>
-						<tr><td style="line-height: 0;" colspan="2">&nbsp;<img height="20" title="" alt="" src="data:image/gif;base64,R0lGODlhAQABAJEAAAAAAP///////wAAACH5BAEAAAIALAAAAAABAAEAAAICVAEAOw%3D%3D"></td></tr>	
-			
-			<tr><td colspan="2" style="font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;line-height: 15px;">На <a style="color: #d44234;" href="<?php echo CI::base('http') ?>">yarmarka.biz</a> произошли следующие действия с вашими объявлениями:</td></tr>
-			<tr>
-				<td colspan="2" style="font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;line-height: 15px;">
-					<? foreach($actions as $action) {?>
-						<p class="p3"><?=$action; ?></p>
-					<? }?>		
-				</td>
-			</tr>
-			
+	<div class="email_cont" style="max-width: 800px; margin: 0 auto; padding: 0px 15px;">
+		<div class="header" style="background: #ECF3F7; padding: 10px;">
+			<div class="logo" style="display: inline-block; font-size: 11px; width: 50%;">
+				<img src="<?=URL::site('static/develop/production/images/logo.png', 'http')?>" alt="Ярмарка" style="width: 160px; height: 31px;"><br>
+				Сайт бесплатных объявлений
+			</div>
+			<div class="site" style="display: inline-block; width: 49%; text-align: right; color: #D44234; text-decoration: none; font-size: 24px;" align="right">
+				yarmarka<span style="color: #5B6772;">.biz</span>
+			</div>
+		</div>
 
-			
-						<tr><td colspan="2" style="line-height: 0;">&nbsp;<img src="data:image/gif;base64,R0lGODlhAQABAJEAAAAAAP///////wAAACH5BAEAAAIALAAAAAABAAEAAAICVAEAOw%3D%3D" height="27" alt="" title=""/></td></tr>
-						<tr><td style="font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;text-align: right;font-size: 12px;" colspan="2">С уважением, команда «Ярмарка-онлайн»</td></tr>			
-			
-						<tr><td colspan="2" style="line-height: 0;"><img src="data:image/gif;base64,R0lGODlhAQABAJEAAAAAAP///////wAAACH5BAEAAAIALAAAAAABAAEAAAICVAEAOw%3D%3D" height="9" alt="" title=""/></td></tr>
-						<tr><td colspan="2" style="font-size: 9px;color: #6c6c6c;font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;">Пожалуйста, не отвечайте на это письмо, т.к. указанный почтовый адрес используется только для рассылки уведомлений</td></tr>
-					</tbody>
-				</table>			
-			</td>
-		</tr>
-   							
-       </tbody>        
-    </table>	
-	
+		<div class="add" style="text-align: right; margin-top: 20px;" align="right">
+			<a href="http://c.yarmarka.biz/add" target="_blank" style="color: #fff; text-decoration: none; background: #D44234; padding: 5px 10px;">Подать объявление</a>
+		</div>
+
+		<div class="content" style="text-align: center; margin-top: 30px;" align="center">
+			<div class="img" style="display: inline-block; width: 100%; max-width: 165px; text-align: center;" align="center">
+				<img src="<?=URL::site('static/develop/production/images/2_165.jpg', 'http')?>">
+		</div>
+			<div class="text" style="text-align: left; line-height: 1.6; display: inline-block; width: 100%; max-width: 630px; margin-bottom: 15px;" align="left">
+				<h2 style="font-size: 1.5em; display: block; text-align: center; margin-bottom: 15px;" align="center">Здравствуйте!</h2>
+				На <a style="color: #d44234;" href="<?php echo CI::base('http') ?>">yarmarka.biz</a> произошли следующие действия с вашими объявлениями: <br><br>
+				<? foreach($actions as $action) {?>
+					<p class="p3"><?=$action; ?></p>
+				<? }?>
+				<div class="footer" style="text-align: right; font-size: 12px; margin: 10px 0;" align="right">С уважением, команда «Ярмарка-онлайн»</div>
+				<span style="color: #6c6c6c; font-size: 9px;">Пожалуйста, не отвечайте на это письмо, т.к. указанный почтовый адрес используется только для рассылки уведомлений</span>
+			</div>
+		</div>
+	</div>
 </body>  
 </html>
