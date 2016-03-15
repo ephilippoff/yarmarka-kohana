1fab3381012136590035c07318ae60c6de48f0e8
almaz vagapov <almaznv@gmail.com>
2016-03-15 21:12:35 +0300

    remvoe newspaper service yarmarka-676

--- application/views/user/myads.php
+++ application/views/user/myads.php
@@ -398,9 +398,6 @@
 													<li><a title="Текстовая ссылка" href="<?=CI::site('billing/pay_service/'.$service_promo_link->id.'/'.$ad->id)?>" class="btn-pmenu "><i class="ico pl"></i><span>Текст. ссылка</span></a></li>
 													<li><a title="Графическая ссылка" href="<?=CI::site('billing/pay_service/'.$service_promo_link_bg->id.'/'.$ad->id)?>" class="btn-pmenu "><i class="ico plg"></i><span>Графич. ссылка</span></a></li>													
 													<?php ?>
-													<?php if (!isset($ad_to_paper_locked_rubrics[$ad->city_id]) or !in_array($ad->category, $ad_to_paper_locked_rubrics[$ad->city_id])) : ?>
-														<li><a title="Подать в газету" href="<?=CI::site('billing/pay_service/34/'.$ad->id)?>" class="btn-pmenu "><i class="ico yarmarka"></i><span>В газету</span></a></li>		
-													<?php endif ?>
 													<li><a title="Воспользоваться услугами" href="<?=CI::site('billing/services_for_ads/'.$ad->id)?>" class="btn-pmenu "><i class="ico services"></i><span>Пакет услуг</span></a></li>													
 										<?php endif; ?>
 									</ul>
