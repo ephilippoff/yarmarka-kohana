6ac34ee2f7b7b087a0df86b388a5b40593453b03
qwetexac <qwetexac@gmail.com>
2016-05-24 18:23:34 +0300

    831

--- application/classes/Detailpage/Default.php
+++ application/classes/Detailpage/Default.php
@@ -89,6 +89,8 @@ class Detailpage_Default
 
 		$info['similar_search_result'] = Search::getresult($similar_search_query->execute()->as_array());
 
+		shuffle($info['similar_search_result']);
+
 		$info['similar_coords'] = array_map(function($item){
 			return array(
 				"id" => $item["id"],
