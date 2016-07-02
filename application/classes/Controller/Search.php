9ded359617cecfb6fadbe70673521952c2937bf1
qwetexac <qwetexac@gmail.com>
2016-07-02 14:42:18 +0300

    new method

--- application/classes/Controller/Search.php
+++ application/classes/Controller/Search.php
@@ -511,7 +511,9 @@ class Controller_Search extends Controller_Template
 
         // Страница поиска по конкретной организации
         if ($twig->search_filters["user_id"] AND $twig->search_filters["user_id"] > 0) {
+
             $twig->orginfo = $this->get_orginfo($twig->search_filters["user_id"]);
+            $twig->category_childs = $this->get_company_categories($twig->search_filters);
 
             $orgname = $twig->orginfo["name"];
             $city = $twig->city->sinonim;
@@ -519,8 +521,25 @@ class Controller_Search extends Controller_Template
             $twig->seo_attributes["h1"] = "Все объявления компании ".$orgname." в ".$city;
         }
 
+
+
         
         $this->response->body($twig);
+    }
+
+    public function get_company_categories($filters)
+    {
+        $filters["category_id"] = NULL;
+        $filters["not_category_seo_name"] = NULL;
+        $categories = Search::searchquery($filters, array(), array("group_category" => TRUE))
+                                                    ->execute()
+                                                    ->as_array();
+        foreach ($categories as &$item) {
+            $item['url'] = $item['url']."/?user_id=".$filters['user_id'];
+        }
+        unset($item);
+
+        return $categories;
     }
 
     public function get_orginfo($id)
