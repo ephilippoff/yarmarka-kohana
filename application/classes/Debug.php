e20b4fbfb5e87e3c6c2aa10e97dcbbe4cedc0321
almaz vagapov <almaznv@gmail.com>
2016-08-30 18:40:04 +0300

    fix

--- application/classes/Debug.php
+++ application/classes/Debug.php
@@ -3,18 +3,18 @@
 	class Debug extends Kohana_Debug {
 
 		public static function email($view) {
-			$sLogFileName = 'email.log';
-			$sLogFilePath = dirname($_SERVER['DOCUMENT_ROOT']) . DIRECTORY_SEPARATOR . $sLogFileName;
-			$rFile = fopen($sLogFilePath, 'a+');
-			fwrite($rFile, "New email " . $view . " " . date('d.m.Y H:i:s') . "\n");
-			fwrite($rFile, "short_open_tag = " . ini_get('short_open_tag') . "\n");
-			$sGlobalsDump = '';
-			ob_start();
-			var_dump($GLOBALS);
-			$sGlobalsDump = ob_get_clean();
-			fwrite($rFile, "\$GLOBALS = " . $sGlobalsDump . "\n");
-			fwrite($rFile, "----------------------------------------------------------\n\n\n");
-			fclose($rFile);
+			// $sLogFileName = 'email.log';
+			// $sLogFilePath = dirname($_SERVER['DOCUMENT_ROOT']) . DIRECTORY_SEPARATOR . $sLogFileName;
+			// $rFile = fopen($sLogFilePath, 'a+');
+			// fwrite($rFile, "New email " . $view . " " . date('d.m.Y H:i:s') . "\n");
+			// fwrite($rFile, "short_open_tag = " . ini_get('short_open_tag') . "\n");
+			// $sGlobalsDump = '';
+			// ob_start();
+			// var_dump($GLOBALS);
+			// $sGlobalsDump = ob_get_clean();
+			// fwrite($rFile, "\$GLOBALS = " . $sGlobalsDump . "\n");
+			// fwrite($rFile, "----------------------------------------------------------\n\n\n");
+			// fclose($rFile);
 		}
 
 	}
