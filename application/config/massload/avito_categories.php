f77861f2faaaba13218b8a9269f2e14bdc13dc91
almaz <almaz@almaz.comp>
2017-01-09 19:47:03 +0300

    fix yarmarka-1041

--- application/config/massload/avito_categories.php
+++ application/config/massload/avito_categories.php
@@ -5,7 +5,7 @@ return array(
 	'flat_rooms'		=> array( "Category" => "Комнаты",  "OperationType" => "Продам"),
 	'flat_new'			=> array( "Category" => "Квартиры", "MarketType" => "Новостройка", "OperationType" => "Продам"),
 	'flat_rent'			=> array( "Category" => "Квартиры", "OperationType" => "Сдам"),
-	'house'				=> array( "Category" => "Дома, дачи, коттеджи",  "OperationType" => "Продам"),
+	'house'				=> array( "Category" => "Дома, дачи, коттеджи"),
 	'land'				=> array( "Category" => "Земельные участки",  "OperationType" => "Продам"),
 	'commercial'		=> array( "Category" => "Коммерческая недвижимость"),
 	'garazhi_i_mashinomesta' => array( "Category" => "Гаражи и машиноместа")
