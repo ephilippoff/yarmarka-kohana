be8bb97a398250aa7ddc2206dc57e31f1e6a67c7
almaz <almaz@almaz.comp>
2017-01-18 21:53:17 +0300

    fix

--- application/classes/Task/Objectload.php
+++ application/classes/Task/Objectload.php
@@ -236,14 +236,12 @@ class Task_Objectload extends Minion_Task
 		ORM::factory('Objectload', $ol->_objectload_id)
 			->update_statistic();
 
-		// if (!$test)
-		// {
-		// 	try {
-		// 		Request::factory('user/send_report/'.$ol->_objectload_id)->execute();
-		// 	} catch (Exception $e){
-		// 		Minion::write($prefix_log, 'Ошибка отпарвки отчета: '.$e->getMessage());
-		// 	}
-		// }
+		if (!$test)
+		{
+
+			$ol->sendReport();
+			Minion::write("Send report", 'Success');
+		}
 
 		Minion::write("Success", 'End');
 		return $ol->_objectload_id;
--- application/classes/Task/Test.php
+++ application/classes/Task/Test.php
@@ -4,24 +4,15 @@
 class Task_Test extends Minion_Task
 {
 	protected $_options = array(
-		"number" => NULL,
-		"code" => NULL
+		"id" => NULL
 	);
 
 	protected function _execute(array $params)
 	{
-		$items = ORM::factory('Order_Item')->where('service_name','IS',NULL)->find_all();
 
-		foreach ($items as $item) {
-			 
-			$data = json_decode($item->params);
 
-			Minion_CLI::write( $data->service->name );
-			$oi = ORM::factory('Order_Item',$item->id);
-			$oi->service_name = $data->service->name;
-			$oi->update();
-		}
-
+		$objectload = new Objectload(NULL, $params['id']);
+		$objectload->sendReport($objectload_id);
 	}
 
 }
